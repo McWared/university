a87dd73984e681f9a7a29d5a9041c4c02fcc58bd
McWared <mrbaymiks@gmail.com>
2023-10-17 23:39:58 +0300

    fixed doc strings

--- Labs/lab1.py
+++ Labs/lab1.py
@@ -1,35 +1,34 @@
 import math
 
-"""
-info about author
-"""
+
 def _info_about_me():
+    """
+    info about author
+    """
     print("The author of this programm is Andrew Pozhylenkov Group K-10 Variant 20.")
     print("This programm calculates the value of the expression by given x.")
 
-"""
-f(x) function calculates the expression
-"""
 def f(x):
-    
+    """
+    f(x) function calculates the expression
+    """
     result = math.cos(22/55) - 2 * math.pi + \
     54 * math.e * 13 / ((x - 4) * (x + 10)) - \
     11 * math.cos(x + 9) + 13 / (x - 5)
 
     return result
 
-"""
-_domain(x) function checks the domain of the expression
-"""
 def _domain(x):
-
+    """
+    _domain(x) function checks the domain of the expression
+    """
     return x!=4 and x!=-10 and x!=5
 
-"""
-main() function gathers all auxiliary functions together and does exception checking
-"""
 def main():
-
+    """
+    main() function gathers all auxiliary functions together and does exception checking
+    """
+    
     _info_about_me()
 
     try:
