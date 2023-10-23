3861f42050b5b1014d035e4eed07638746259682
McWared <mrbaymiks@gmail.com>
2023-10-23 21:58:48 +0300

    DZ_7 done

--- Homework/DZ_7.py
+++ Homework/DZ_7.py
@@ -3,11 +3,39 @@
 Написати рекурсивну функцію, яка за цілим числом визначає кількість входжень заданої цифри в його десятковий запис.
 """
 
-def find_number(value):
+def find_number(value, numToFind):
 
-    numToFind = int(input("Enter number to find (0,1,2,3,4,5,6,7,8,9): "))
+    if value<=0:
+        return 0
+    
+    if value % 10 == numToFind:
+        return 1 + find_number(value // 10, numToFind)
+    else:
+        return find_number(value // 10, numToFind)
+    
+print(find_number(123456654321, 6)) # Output: 2 (correct)
 
-    count = 0
+# Файл 11, номер 10
 
-    if value==0:
-        return 
+"""
+Написати рекурсивну функцію обчислення числа Фібоначчі.
+"""
+
+counter = 0
+
+def fib(num):
+
+    global counter
+    counter += 1
+
+    if num<0:
+        return -1
+    if num==0:
+        return 0
+    if num==1:
+        return 1
+    
+    return fib(num - 1) + fib(num - 2)
+
+print(fib(20)) # Output: 8 (correct)
+print(counter)
